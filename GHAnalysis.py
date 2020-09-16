6fd7d8b057ea72c00f917f184e97957cbcf76b1b
edwardwsb <793082615@qq.com>
2020-09-16 23:43:41 +0800

    保存python代码第二代

--- GHAnalysis.py
+++ GHAnalysis.py
@@ -1,123 +1,87 @@
-import json
 import os
 import argparse
+import pickle
+import re
+from concurrent.futures import ThreadPoolExecutor
 
+EVENTS = ("PushEvent", "IssueCommentEvent", "IssuesEvent", "PullRequestEvent", )
+pattern = re.compile(r'"type":"(\w+?)".*?actor.*?"login":"(\S+?)".*?repo.*?"name":"(\S+?)"')
 
 class Data:
-    def __init__(self, addr: str = None, reload: int = 0):
-        self.uevent = {}
-        self.revent = {}
-        self.urevent = {}
-        if reload == 1:
-            self.read_data(addr)
-            self.savetolocal()
-        if (addr is None and not os.path.exists('1.json')
-        ) and not os.path.exists('2.json') and not os.path.exists('3.json'):
-            raise RuntimeError('error: init failed')
+    def __init__(self):
+        self.login_type = {}
+        self.name_type = {}
+        self.get_tot_ans = {}
 
-        x = open('1.json', 'r', encoding='utf-8').read()
-        self.localu = json.loads(x)
-        x = open('2.json', 'r', encoding='utf-8').read()
-        self.localr = json.loads(x)
-        x = open('3.json', 'r', encoding='utf-8').read()
-        self.localur = json.loads(x)
+    def count_ans(self, filename: str):
+        with open(filename, 'r', encoding='utf-8') as f:
+            for line in f:
+                res = pattern.search(line)
+                if res is None or res[1] not in EVENTS:
+                    continue
 
-    def read_data(self, addr: str):
-        for root, dic, files in os.walk(addr):
-            for f in files:
-                if f[-5:] == ".json":
-                    jpath = f
-                    self.analyse(addr, jpath)
+                event, user, repo = res.groups()
+                self.login_type.setdefault(user, {})
+                self.get_tot_ans.setdefault(user, {})
+                self.name_type.setdefault(repo, {})
+                self.get_tot_ans[user].setdefault(repo, {})
 
-    def analyse(self, addr: str, jpath: str):
-        f = open(addr + '\\' + jpath, 'r', encoding='utf-8')
-        try:
-            while True:
-                stmp = f.readline()
-                if stmp:
-                    dtmp = json.loads(stmp)
-                    if not dtmp["type"] in ['PushEvent', 'IssueCommentEvent','IssuesEvent', 'PullRequestEvent']:
-                        continue
-                    if not dtmp["actor"]["login"] in self.uevent.keys():
-                        event = {'PushEvent': 0, 'IssueCommentEvent': 0, 'IssuesEvent': 0, 'PullRequestEvent': 0}
-                        self.uevent[dtmp["actor"]["login"]] = event
-                    if not dtmp["repo"]["name"] in self.revent.keys():
-                        event = {'PushEvent': 0, 'IssueCommentEvent': 0,'IssuesEvent': 0, 'PullRequestEvent': 0}
-                        self.revent[dtmp["repo"]["name"]] = event
-                    if not dtmp["actor"]["login"] + dtmp["repo"]["name"] in self.urevent.keys():
-                        event = {'PushEvent': 0, 'IssueCommentEvent': 0,'IssuesEvent': 0, 'PullRequestEvent': 0}
-                        self.urevent[dtmp["actor"]["login"] + dtmp["repo"]["name"]] = event
+                self.login_type[user][event] = self.login_type[user].get(event, 0)+1
+                self.name_type[repo][event] = self.name_type[repo].get(event, 0)+1
+                self.get_tot_ans[user][repo][event] = self.get_tot_ans[user][repo].get(event, 0)+1
 
-                    self.uevent[dtmp["actor"]["login"]][dtmp['type']] += 1
-                    self.revent[dtmp["repo"]["name"]][dtmp['type']] += 1
-                    self.urevent[dtmp["actor"]["login"] + dtmp["repo"]["name"]][dtmp['type']] += 1
-                else:
-                    break
-        except:
-            pass
-        finally:
-            f.close()
+    def init(self, dir_path: str):
+        pool = ThreadPoolExecutor()
+        for cur_dir, sub_dir, filenames in os.walk(dir_path):
+            filenames = filter(lambda r: r.endswith('.json'), filenames)
+            for name in filenames:
+                pool.submit(self.count_ans, f'{cur_dir}/{name}')
+        pool.shutdown()
 
-    def savetolocal(self):
-        with open('1.json', 'w', encoding='utf-8') as f:
-            json.dump(self.uevent, f)
-        with open('2.json', 'w', encoding='utf-8') as f:
-            json.dump(self.revent, f)
-        with open('3.json', 'w', encoding='utf-8') as f:
-            json.dump(self.urevent, f)
+        with open('1.json', 'wb') as f:
+            pickle.dump(self.login_type, f)
+        with open('2.json', 'wb') as f:
+            pickle.dump(self.name_type, f)
+        with open('3.json', 'wb') as f:
+            pickle.dump(self.get_tot_ans, f)
 
-    def query_u(self, user: str, event: str):
-        if not user in self.localu.keys():
-            return 0
-        return self.localu[user][event]
+    def load(self):
+        if not any((os.path.exists(f'{i}.json') for i in range(1, 4))):
+            raise RuntimeError('error: data file not found')
 
-    def query_r(self, repo: str, event: str):
-        if not self.localr.get(repo, 0):
-            return 0
-        return self.localr[repo][event]
-
-    def query_ur(self, user: str, repo: str, event: str):
-        if not self.localur.get(user + repo, 0):
-            return 0
-        return self.localur[user + repo][event]
+        with open('1.json', 'rb') as f:
+            self.login_type = pickle.load(f)
+        with open('2.json', 'rb') as f:
+            self.name_type = pickle.load(f)
+        with open('3.json', 'rb') as f:
+            self.get_tot_ans = pickle.load(f)
 
 
 class Run:
     def __init__(self):
+        self.data = Data()
         self.parser = argparse.ArgumentParser()
-        self.data = None
-        self.parser.add_argument('-i', '--init')
-        self.parser.add_argument('-u', '--user')
-        self.parser.add_argument('-r', '--repo')
-        self.parser.add_argument('-e', '--event')
+        self.parser.add_argument('-i', '--init', type=str)
+        self.parser.add_argument('-u', '--user', type=str)
+        self.parser.add_argument('-r', '--repo', type=str)
+        self.parser.add_argument('-e', '--event', type=str)
         print(self.analyse())
 
     def analyse(self):
-        if self.parser.parse_args().init:
-            self.data = Data(self.parser.parse_args().init, 1)
-            return 0
+        args = self.parser.parse_args()
+        event, user, repo = args.event, args.user, args.repo
+
+        if args.init:
+            self.data.init(args.init)
+            return 'init done'
+        self.data.load()
+
+        if user and repo:
+            res = self.data.get_tot_ans.get(user, {}).get(repo, {}).get(event, 0)
+        elif user:
+            res = self.data.login_type.get(user, {}).get(event, 0)
         else:
-            if self.data is None:
-                self.data = Data()
-            if self.parser.parse_args().event:
-                if self.parser.parse_args().user:
-                    if self.parser.parse_args().repo:
-                        res = self.data.query_ur(
-                            self.parser.parse_args().user,
-                            self.parser.parse_args().repo,
-                            self.parser.parse_args().event)
-                    else:
-                        res = self.data.query_u(
-                            self.parser.parse_args().user,
-                            self.parser.parse_args().event)
-                elif self.parser.parse_args().repo:
-                    res = self.data.query_r(
-                        self.parser.parse_args().repo,
-                        self.parser.parse_args().event)
-                else:
-                    raise RuntimeError('error: argument -u or -r are required')
-            else:
-                raise RuntimeError('error: argument -e is required')
+            res = self.data.name_type.get(repo, {}).get(event, 0)
         return res
 
 
